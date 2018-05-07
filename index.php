888a0e6b307ae806d9206e968821057bcd82d9b3
AdrianR719 <arenteria80@uabc.edu.mx>
2018-05-06 17:21:46 -0700

    Funcionando con micro servicio proyectos

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-                                include 'ConexionDB.php';
-                                $conexion = new Conexion_DB();
-                                $conexion->connect();
-        echo 'listo';
+
         
 include_once("home.html"); ?>
